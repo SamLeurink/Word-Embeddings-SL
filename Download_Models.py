bfc2e4f93e32d202ec61c030cb32ec4deb986635
SamLeurink <84024082+SamLeurink@users.noreply.github.com>
2022-11-11 13:20:23 +0100

    Update Download_Models.py

--- Download_Models.py
+++ Download_Models.py
@@ -3,17 +3,24 @@ import gensim.models
 import gensim.downloader
 from scipy.spatial import distance
 
+#Loading the google news model from gensim
 google_news_vectors = gensim.downloader.load('word2vec-google-news-300')
 
-with open(r"C:\Users\samle\Documents\NRC-VAD-Lexicon-Aug2018Release\NRC-VAD-Lexicon-Aug2018Release\NRC-VAD-Lexicon.txt") as f:
+#Open the text file containing the NRC-VAD lexicon
+with open("NRC-VAD-Lexicon.txt") as f:
     contents = f.readlines()
 
+#Create a list containing only the first words in the text file.
 thelist = []
 for x in contents:
     pl = x.split()
     thelist.append(pl[0])
 
+#Creating a list of the seed words for convenience
 seedlist = ['good','bad','active','passive', 'dominant', 'submissive']
+
+#Creating a csv file containing the cosine similarities between the words in thelist and the seedwords, if a specific word doesn't return a value it will
+#return an empty line
 with open('resultscs.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['name','good','bad', 'active', 'passive', 'dominant', 'submissive'])
@@ -27,6 +34,7 @@ with open('resultscs.csv', 'w') as f:
                 pass
         writer.writerow(therow)
 
+#Similar to cosine similarity, this creates a csv file containing the euclidean distances between the words in thelist and the seedwords. 
 with open('resultsed.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['name','good','bad', 'active', 'passive', 'dominant', 'submissive'])
